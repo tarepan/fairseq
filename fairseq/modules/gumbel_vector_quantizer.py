5df15c480ab73a9e40c5c96717dd5148641428d2
tarepan <tarepan5884@gmail.com>
2021-12-08 19:05:43 +0000

    Add commentary of Gambel-softmax

--- fairseq/modules/gumbel_vector_quantizer.py
+++ fairseq/modules/gumbel_vector_quantizer.py
@@ -11,27 +11,31 @@ import torch.nn.functional as F
 class GumbelVectorQuantizer(nn.Module):
     def __init__(
         self,
-        dim,
-        num_vars,
+        dim:int,
+        num_vars:int,
         temp,
-        groups,
+        groups:int,
         combine_groups,
-        vq_dim,
-        time_first,
+        vq_dim:int,
+        time_first:bool,
         activation=nn.GELU(),
-        weight_proj_depth=1,
+        weight_proj_depth:int=1,
         weight_proj_factor=1,
     ):
         """Vector quantization using gumbel softmax
 
+        Multiple methodologies are supported.
+            - Product Quantization (Grouping)
+            - Temperature decay
+
         Args:
             dim: input dimension (channels)
-            num_vars: number of quantized vectors per group
+            num_vars: number of quantized vectors per group (size of sub-codebook; `V`)
             temp: temperature for training. this should be a tuple of 3 elements: (start, stop, decay factor)
-            groups: number of groups for vector quantization
+            groups: number of groups for vector quantization; `G`
             combine_groups: whether to use the vectors for all groups
             vq_dim: dimensionality of the resulting quantized vector
-            time_first: if true, expect input in BxTxC format, otherwise in BxCxT
+            time_first: input:(Batch, Time, Channel) if True else (Batch, Channel, Time)
             activation: what activation to use (should be a module). this is only used if weight_proj_depth is > 1
             weight_proj_depth: number of layers (with activation in between) to project input before computing logits
             weight_proj_factor: this is used only if weight_proj_depth is > 1. scales the inner dimensionality of
@@ -49,15 +53,21 @@ class GumbelVectorQuantizer(nn.Module):
             vq_dim % groups == 0
         ), f"dim {vq_dim} must be divisible by groups {groups} for concatenation"
 
+        # Dimension of each group's representative vector
         var_dim = vq_dim // groups
         num_groups = groups if not combine_groups else 1
 
+        # Full Codebook: (1, G*V, var_dim)
         self.vars = nn.Parameter(torch.FloatTensor(1, num_groups * num_vars, var_dim))
         nn.init.uniform_(self.vars)
 
+        # Projection: Linear/non-Linear projection for dimension matching
+        #     depth>1: vec_i::(B, T, C) -> [FC_{factor}-σ]x{depth} -> (B, T, G*V)
+        #     depth=1: vec_i::(B, T, C) -> Linear -> (B, T, G*V)
         if weight_proj_depth > 1:
 
             def block(input_dim, output_dim):
+                """FC-σ"""
                 return nn.Sequential(nn.Linear(input_dim, output_dim), activation)
 
             inner_dim = self.input_dim * weight_proj_factor
@@ -72,18 +82,28 @@ class GumbelVectorQuantizer(nn.Module):
             self.weight_proj = nn.Linear(self.input_dim, groups * num_vars)
             nn.init.normal_(self.weight_proj.weight, mean=0, std=1)
             nn.init.zeros_(self.weight_proj.bias)
+        # /
 
+        # Gumbel-softmax Temperature
         if isinstance(temp, str):
             import ast
-
             temp = ast.literal_eval(temp)
         assert len(temp) == 3, f"{temp}, {len(temp)}"
-
         self.max_temp, self.min_temp, self.temp_decay = temp
+        ## Current temperature, initialized with maximum
         self.curr_temp = self.max_temp
+        # /
+
         self.codebook_indices = None
 
     def set_num_updates(self, num_updates):
+        """(maybe) Cool sampler.
+
+        Lower the temperature of sampler.
+        This procedure is idempotent (Call twice do NOT cool two times, there is no cooling history)
+        Args:
+            num_updates: Number of cool/decay
+        """
         self.curr_temp = max(
             self.max_temp * self.temp_decay ** num_updates, self.min_temp
         )
@@ -110,7 +130,9 @@ class GumbelVectorQuantizer(nn.Module):
     def codebook(self):
         indices = self.get_codebook_indices()
         return (
+            # (1, G*V, var_dim) =>  (G*V, var_dim)
             self.vars.squeeze(0)
+            # (G*V, var_dim) => (dim_indices, var_dim)
             .index_select(0, indices)
             .view(self.num_vars ** self.groups, -1)
         )
@@ -136,21 +158,50 @@ class GumbelVectorQuantizer(nn.Module):
         return res
 
     def forward_idx(self, x):
+        """Forward with produce_targets flag, then return `x` and `target`
+
+        Args:
+            x: Input (Batch, Time, Feature) if time_first else (Batch, Feature, Time)
+        Returns:
+            (sampled vector series, ?)
+        """
         res = self.forward(x, produce_targets=True)
         return res["x"], res["targets"]
 
     def forward(self, x, produce_targets=False):
+        """
+        Quantize input feature sequences.
 
+        Args:
+            x: Input (Batch, Time, Feature) if time_first else (Batch, Feature, Time)
+        Returns:
+            Dict {
+            'num_vars': Number of sub-codes (V*G)
+            'code_perplexity'
+            'prob_perplexity'
+            'temp': Temperature at sampling
+            'x' (B, T, vq_dim==G*var_dim): Sampled representative vector series
+            'targets' Optional[(B, T, G)]: One-hot vector series for training targets
+        }
+        """
+
+        # Output dictionary
         result = {"num_vars": self.num_vars * self.groups}
 
+        # ((Batch, Time, Feature) | (Batch, Feature, Time)) => (B, T, F)
         if not self.time_first:
             x = x.transpose(1, 2)
 
+        # B, T, F
         bsz, tsz, fsz = x.shape
+        # Reshape because quantization is done toward each vector: (B, T, F) => (B*T, F)
         x = x.reshape(-1, fsz)
+        # Feature projection: (B*T, F) => (B*T, G*V)
         x = self.weight_proj(x)
+        # Grouping: (B*T, G*V) => (B*T*G, V)
         x = x.view(bsz * tsz * self.groups, -1)
 
+        # Undifferentiable sampling: Argmax
         _, k = x.max(-1)
         hard_x = (
             x.new_zeros(*x.shape)
@@ -161,6 +212,7 @@ class GumbelVectorQuantizer(nn.Module):
         result["code_perplexity"] = torch.exp(
             -torch.sum(hard_probs * torch.log(hard_probs + 1e-7), dim=-1)
         ).sum()
+        # /
 
         avg_probs = torch.softmax(
             x.view(bsz * tsz, self.groups, -1).float(), dim=-1
@@ -169,34 +221,47 @@ class GumbelVectorQuantizer(nn.Module):
             -torch.sum(avg_probs * torch.log(avg_probs + 1e-7), dim=-1)
         ).sum()
 
+        # Output 4: Temperature
         result["temp"] = self.curr_temp
 
+        # Differential hard random sampling
         if self.training:
+            # (B*T*G, V) => (B*T*G, V), last dim is one-hot vector
             x = F.gumbel_softmax(x.float(), tau=self.curr_temp, hard=True).type_as(x)
+        # Differential is not necessary, so Argmax sampling is enough.
         else:
             x = hard_x
 
+        # degrouping/concat: (B*T*G, V) => (B*T, G*V)
         x = x.view(bsz * tsz, -1)
 
+        # (1, G*V, var_dim)
         vars = self.vars
         if self.combine_groups:
             vars = vars.repeat(1, self.groups, 1)
 
+        # Produce one-hot vectors for training target
         if produce_targets:
             result["targets"] = (
+                # (B*T, G*V) => (B*T*G, V)
                 x.view(bsz * tsz * self.groups, -1)
+                # (B*T*G, V) => (B*T*G), values are index
                 .argmax(dim=-1)
+                # (B*T*G) => (B, T, G)
                 .view(bsz, tsz, self.groups)
                 .detach()
             )
 
+        # one-hot vectors to representative vectors: (B*T, G*V, 1) * (1, G*V, var_dim) => (B*T, G*V, var_dim)
+        # Most row is zero vector, hot rows are converted to the vectors, so G non-zero rows & G(V-1) zero rows
         x = x.unsqueeze(-1) * vars
-        x = x.view(bsz * tsz, self.groups, self.num_vars, -1)
-        x = x.sum(-2)
-        x = x.view(bsz, tsz, -1)
+        # Squash zero raws in each groups, then become concatenated representative vectors
+        #     (B*T, G*V, var_dim) => (B*T, G, V, var_dim) => (B*T, G, var_dim) => (B, T, G*var_dim == vq_dim)
+        x = x.view(bsz * tsz, self.groups, self.num_vars, -1).sum(-2).view(bsz, tsz, -1)
 
+        # (B, T, vq_dim) => ((B, T, vq_dim) | (B, vq_dim, T)) same as input
         if not self.time_first:
-            x = x.transpose(1, 2)  # BTC -> BCT
+            x = x.transpose(1, 2)
 
         result["x"] = x
 
